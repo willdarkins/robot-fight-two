4a2e38d135a98400216f517c318882814a01e4b1
Will Darkins <willdarkins@gmail.com>
2022-02-10 20:42:10 -0800

    basic game flow complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,9 +8,22 @@ let enemyHealth = 50;
 let enemyAttack = 12;
 
 const fight = () => {
-    window.alert('Welcome to Robot Galdiators ' + playerName + '... You little bitch!')
+    window.alert('Welcome to Robot Galdiators ' + playerName + '👋... You little bitch!')
     enemyHealth -= playerAttack;
-    console.log(playerName + ' attacked! ' + enemyName + ' now has ' + enemyHealth + ' health points remaining.')
+    window.alert(playerName + ' attacked! ' + enemyName + ' now has ' + enemyHealth + ' health points remaining.')
+    playerHealth -= enemyAttack;
+    window.alert(enemyName + ' attacked! ' + playerName + ' now has ' + playerHealth + ' health points remaining.')
+    if(enemyName <= 0){
+        window.alert(enemyName + ' has died! BUUUUUMMER!')
+    } else {
+        window.alert(enemyName + ' still has ' + enemyHealth + ' health points remaining.')
+    }
+    if(playerHealth > 0){
+        window.alert(playerName + ' still has ' + playerHealth + ' health points remaining, and is still alive!')
+    } else {
+        window.alert(playerName + ' is dead as fucking door nail 💀')
+    }
+
 }
 
 fight()
