cb674348d6cd9abeb0992f3db0b6edc45a968866
Will Darkins <willdarkins@gmail.com>
2022-02-10 20:30:58 -0800

    added some variable names and stuff

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,16 @@
-const playerName = window.prompt('What is your player\'s name?')
-const playerHealth = 100;
-const playerAttack = 10;
+let playerName = window.prompt('What is your player\'s name?')
+let playerHealth = 100;
+let playerAttack = 10;
 
 
-const enemyName = 'Roborto';
-const enemyHelath = 50;
-const enemyAttack = 12;
+let enemyName = 'Roborto';
+let enemyHealth = 50;
+let enemyAttack = 12;
 
 const fight = () => {
     window.alert('Welcome to Robot Galdiators ' + playerName + '... You little bitch!')
+    enemyHealth -= playerAttack;
+    console.log(playerName + ' attacked! ' + enemyName + ' now has ' + enemyHealth + ' health points remaining.')
 }
 
 fight()
