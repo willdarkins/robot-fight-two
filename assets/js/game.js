69b9c3914de22d1174e287ca6e5d72d32e632dd8
Will Darkins <willdarkins@gmail.com>
2022-02-10 20:22:32 -0800

    basic name varibales and main fight function created

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,14 @@
-const playerName = window.prompt('What is your robot\'s name?')
+const playerName = window.prompt('What is your player\'s name?')
+const playerHealth = 100;
+const playerAttack = 10;
 
-window.alert(playerName)
 
-//fight('Will')
+const enemyName = 'Roborto';
+const enemyHelath = 50;
+const enemyAttack = 12;
 
+const fight = () => {
+    window.alert('Welcome to Robot Galdiators ' + playerName + '... You little bitch!')
+}
+
+fight()
