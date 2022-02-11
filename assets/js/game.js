17b7f08d0070b3e3563b520a3744018f9a3b20a0
Will Darkins <willdarkins@gmail.com>
2022-02-10 21:00:36 -0800

    added conditionals to the fight function that gave someone a bail out if they wanted it

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,8 @@
 let playerName = window.prompt('What is your player\'s name?')
+
 let playerHealth = 100;
 let playerAttack = 10;
+let playerMoney = 20;
 
 
 let enemyName = 'Roborto';
@@ -9,20 +11,35 @@ let enemyAttack = 12;
 
 const fight = () => {
     window.alert('Welcome to Robot Galdiators ' + playerName + '👋... You little bitch!')
-    enemyHealth -= playerAttack;
-    window.alert(playerName + ' attacked! ' + enemyName + ' now has ' + enemyHealth + ' health points remaining.')
-    playerHealth -= enemyAttack;
-    window.alert(enemyName + ' attacked! ' + playerName + ' now has ' + playerHealth + ' health points remaining.')
-    if(enemyName <= 0){
-        window.alert(enemyName + ' has died! BUUUUUMMER!')
+    let fightPrompt = window.prompt('Would you like to FIGHT or SKIP this battle? Enter \'FIGHT\' or \'SKIP\' to choose.')
+    if (fightPrompt === 'FIGHT' || fightPrompt === 'fight') {
+        enemyHealth -= playerAttack;
+        window.alert(playerName + ' attacked! ' + enemyName + ' now has ' + enemyHealth + ' health points remaining.')
+        playerHealth -= enemyAttack;
+        window.alert(enemyName + ' attacked! ' + playerName + ' now has ' + playerHealth + ' health points remaining.')
+        if (enemyName <= 0) {
+            window.alert(enemyName + ' has died! BUUUUUMMER!')
+        } else {
+            window.alert(enemyName + ' still has ' + enemyHealth + ' health points remaining.')
+        }
+        if (playerHealth > 0) {
+            window.alert(playerName + ' still has ' + playerHealth + ' health points remaining, and is still alive!')
+        } else {
+            window.alert(playerName + ' is dead as fucking door nail 💀')
+        }
+    } else if (fightPrompt === 'SKIP' || fightPrompt === 'skip') {
+        let confirmSkip = window.confirm('You sure you want to bitch out?')
+        if (confirmSkip) {
+            playerMoney -= 2
+            window.alert(playerName + ' proved they are indeed a little bitch, and skipped this fight... pussy... 2 player moneies gone for you! ' + playerName + ' now has ' + playerMoney + ' player money remaining.')
+        } else {
+            fight();
+        }
     } else {
-        window.alert(enemyName + ' still has ' + enemyHealth + ' health points remaining.')
+        window.alert('Please pick a viable option')
+        fight()
     }
-    if(playerHealth > 0){
-        window.alert(playerName + ' still has ' + playerHealth + ' health points remaining, and is still alive!')
-    } else {
-        window.alert(playerName + ' is dead as fucking door nail 💀')
-    }
+
 
 }
 
