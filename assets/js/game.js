953950c4f50a9f9ead8ad8b04bdd77481929c6ae
Will Darkins <willdarkins@gmail.com>
2022-02-10 21:39:39 -0800

    added some more conditional statements to shore up the game play

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,37 +9,45 @@ let enemyHealth = 50;
 let enemyAttack = 12;
 
 const fight = (enemyName) => {
-    window.alert('Welcome to Robot Galdiators ' + playerName + '👋... You little bitch!')
-    let fightPrompt = window.prompt('Would you like to FIGHT or SKIP this battle? Enter \'FIGHT\' or \'SKIP\' to choose.')
-    if (fightPrompt === 'FIGHT' || fightPrompt === 'fight') {
-        enemyHealth -= playerAttack;
-        window.alert(playerName + ' attacked! ' + enemyName + ' now has ' + enemyHealth + ' health points remaining.')
-        playerHealth -= enemyAttack;
-        window.alert(enemyName + ' attacked! ' + playerName + ' now has ' + playerHealth + ' health points remaining.')
-        if (enemyName <= 0) {
-            window.alert(enemyName + ' has died! BUUUUUMMER!')
+    while (enemyHealth > 0 && playerHealth > 0) {
+        // window.alert('Welcome to Robot Galdiators ' + playerName + '👋... You little bitch!')
+        let fightPrompt = window.prompt('Would you like to FIGHT or SKIP this battle? Enter \'FIGHT\' or \'SKIP\' to choose.')
+        if (fightPrompt === 'FIGHT' || fightPrompt === 'fight') {
+            enemyHealth -= playerAttack;
+            window.alert(playerName + ' attacked! ' + enemyName + ' now has ' + enemyHealth + ' health points remaining.')
+            playerHealth -= enemyAttack;
+            window.alert(enemyName + ' attacked! ' + playerName + ' now has ' + playerHealth + ' health points remaining.')
+            if (enemyHealth <= 0) {
+                window.alert(enemyName + ' has died! BUUUUUMMER!')
+                break;
+            } else {
+                window.alert(enemyName + ' still has ' + enemyHealth + ' health points remaining.')
+            }
+            if (playerHealth > 0) {
+                window.alert(playerName + ' still has ' + playerHealth + ' health points remaining, and is still alive!')
+            } else {
+                window.alert(playerName + ' is dead as a fucking door nail 💀')
+                break;
+            }
+        } else if (fightPrompt === 'SKIP' || fightPrompt === 'skip') {
+            let confirmSkip = window.confirm('You sure you want to bitch out?')
+            if (confirmSkip) {
+                playerMoney -= 10
+                window.alert(playerName + ' proved they are indeed a little bitch, and skipped this fight... pussy... 10 player moneies gone for you! ' + playerName + ' now has ' + playerMoney + ' player money remaining.')
+                break;
+            } else {
+                fight();
+            }
         } else {
-            window.alert(enemyName + ' still has ' + enemyHealth + ' health points remaining.')
+            window.alert('Please pick a viable option')
+            fight()
         }
-        if (playerHealth > 0) {
-            window.alert(playerName + ' still has ' + playerHealth + ' health points remaining, and is still alive!')
-        } else {
-            window.alert(playerName + ' is dead as a fucking door nail 💀')
-        }
-    } else if (fightPrompt === 'SKIP' || fightPrompt === 'skip') {
-        let confirmSkip = window.confirm('You sure you want to bitch out?')
-        if (confirmSkip) {
-            playerMoney -= 2
-            window.alert(playerName + ' proved they are indeed a little bitch, and skipped this fight... pussy... 2 player moneies gone for you! ' + playerName + ' now has ' + playerMoney + ' player money remaining.')
-        } else {
-            fight();
-        }
-    } else {
-        window.alert('Please pick a viable option')
-        fight()
     }
+
 }
 
 for (let i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames.i)
+    let pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName)
 }
